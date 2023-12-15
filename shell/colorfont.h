3ba1d27b9b360e33242657a5ecc27fa2bcca3bb4
sbming <emoming@outlook.com>
2023-12-15 07:44:36 -0800

    Update colorfont.h

--- shell/colorfont.h
+++ shell/colorfont.h
@@ -1,8 +1,10 @@
 #ifndef _COLORFONT_H_
 #define _COLORFONT_H_
 
-#define COLOR_END "\033[0m"
-#define COLOR_BLK "
+#define COLOR_END "\x1B[0m"
+#define COLOR_BLK "\x1B[1;30"
+#define COLOR_RED "\x1B[1:31"
+#define COLOR_END "\x
 
 
 
