ddcdb72a695a3539f9cbfab7cb2ec023d575df2b
sbming <emoming@outlook.com>
2023-12-16 07:56:25 -0800

    Update test.c

--- shell/test.c
+++ shell/test.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stddef.h>
-
+#include <stdlib.h>
 int main() {
 
-	wchar_t c = L'└';
-	for(int i = 0; i < 20; i++) {
-		printf("%lc %lc\n", c+i, c-i);
-	}
-	return 0;
+	char* path = realpath(".", NULL);
+	printf("%s\n", path);
+	free(path);
+
 }
